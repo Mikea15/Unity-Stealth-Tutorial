775042a24f320247643e001a0d9fa4a3ec9424c8
Michael Adaixo <michael.adaixo@mail.com>
2014-10-21 23:02:36 +0100

    Tags and Screenfader with new UI using animations and animator

---
+++ Assets/Scripts/SceneFadeInOut.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFadeInOut : MonoBehaviour 
+{
+	public float fadeSpeed = 1.5f;
+
+	private bool sceneStarting = true;
+
+	void Awake( )
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void StartScene()
+	{
+		sceneStarting = true;
+	}
+
+	public void EndScene()
+	{
+		sceneStarting = false;
+	}
+
+}
