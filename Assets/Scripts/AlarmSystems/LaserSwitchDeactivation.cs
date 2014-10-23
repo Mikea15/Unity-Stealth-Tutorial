6ead358407a89547219b6380352e257d9f0db071
Michael Adaixo <michael.adaixo@mail.com>
2014-10-23 21:06:30 +0100

    chapter1 done :)

---
+++ Assets/Scripts/AlarmSystems/LaserSwitchDeactivation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserSwitchDeactivation : MonoBehaviour 
+{
+	public GameObject laser;
+	public Material unlockedMat;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		player = GameObject.FindGameObjectWithTag(Tags.player);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerStay( Collider other ) 
+	{
+		if( other.gameObject == player )
+		{
+			if( Input.GetButton("Switch") )
+			{
+				LaserDeactivation( );
+			}
+		}
+	}
+
+	void LaserDeactivation( )
+	{
+		laser.SetActive (false);
+
+		Renderer screen = transform.Find ("prop_switchUnit_screen_001").renderer;
+		screen.material = unlockedMat;
+		audio.Play();
+	}
+}
---
+++ Assets/Scripts/AlarmSystems/LazerBlinking.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LazerBlinking : MonoBehaviour 
+{
+
+	public float onTime;
+	public float offTime;
+
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime;
+		if( renderer.enabled && timer >= onTime )
+		{
+			SwitchBeam();
+		}
+
+		if( renderer.enabled && timer >= offTime )
+		{
+			SwitchBeam();
+		}
+	}
+
+	void SwitchBeam()
+	{
+		timer = 0.0f;
+
+		renderer.enabled = !renderer.enabled;
+		light.enabled = !light.enabled;
+	}
+}
